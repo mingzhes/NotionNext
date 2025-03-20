87a181db6d59c0d21cc22f3754b7bde39c8ff86d
mingzhes <84654328+mingzhes@users.noreply.github.com>
2025-03-20 15:56:24 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -1,19 +1,19 @@
-import { siteConfig } from '@/lib/config'
+/import { siteConfig } from '@/lib/config'
 
 /**
  * 驱动版权
  * @returns
  */
-export default function PoweredBy(props) {
-  return (
-    <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>Powered by</span>
-      <a
-        href='https://henduoqiao.com'
-        className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
-      </a>
+/export default function PoweredBy(props) {
+  /return (
+    /<div className={`inline text-sm font-serif ${props.className || ''}`}>
+      /<span className='mr-1'>Powered by</span>
+      /<a
+       / href='https://henduoqiao.com'
+        /className='underline justify-start'>
+       / NotionNext {siteConfig('VERSION')}
+     / </a>
       .
-    </div>
-  )
-}
+   / </div>
+ / )
+/}

4e3233ad25dec359879a755a205571b0b9cc7ccc
mingzhes <84654328+mingzhes@users.noreply.github.com>
2025-04-18 23:54:39 +0800

    Update Coze.js
    token

--- components/Coze.js
+++ components/Coze.js
@@ -8,7 +8,7 @@ import { loadExternalResource } from '@/lib/utils'
 export default function Coze() {
   const cozeSrc = 'https://lf-cdn.coze.cn/obj/unpkg/flow-platform/chat-app-sdk/1.2.0-beta.6/libs/cn/index.js'
   const botId = siteConfig('COZE_BOT_ID')
-  const token = siteConfig('COZE_TOKEN')
+  const token = siteConfig('pat_MyGsMf0ZGL3Vks7K2cG01yg8joMop0KH8dk7fyLOd0UZuunNtUHlJxxXrvCN9Uo0')
   const title = siteConfig('COZE_TITLE', 'Coze')
 
   const loadCoze = async () => {
